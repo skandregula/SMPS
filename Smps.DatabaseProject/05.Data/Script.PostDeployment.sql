c93c2bddc73461ae71a7485e3dec9b1e17db1fc3
Satyendra <Satyendra_Kandregula@epam.com>
2018-11-04 22:58:27 +0530

    Update Script.PostDeployment.sql

--- Smps.DatabaseProject/05.Data/Script.PostDeployment.sql
+++ Smps.DatabaseProject/05.Data/Script.PostDeployment.sql
@@ -12,12 +12,7 @@ Post-Deployment Script Template
 
 
 MERGE INTO [dbo].[Users] AS TARGET USING (VALUES
-           (519420,'Sai','Patha',9948484684,'sai_patha@epam.com','Holder','1','JVP',0,'Epam@1234'),
-                 (519280,'sujitha','vemulapally',9959764989,'sujitha_vemulapally@epam.com','Holder','2','JVP',0,'Epam@1234'),
-                 (519180,'sindhuja','rudroju',9948484684,'sindhuja_rudroju@epam.com','Holder','3','JVP',0,'Epam@1234'),
-                 (518935,'srinivas','injam',8978859293,'srinivasa_injam@epam.com','Seeker',NULL,'JVP',0,'Epam@1234'),
-                 (519562,'raviteja','thummala',9963862195,'raviteja_thummala@epam.com','Seeker',NULL,'JVP',0,'Epam@1234'),
-                 (519325,'prem','yelavarthi',8977510620,'prem_yelavarthi@epam.com','Holder','6','JVP',0,'Epam@1234')
+           
 				 )
 
 				 AS Source ([EmpNo]
